a1b403cdab19a3498b536c80332fe67fa1734f70
hawk.0fcx <67156465+hawk-unity@users.noreply.github.com>
2021-07-24 14:41:17 -0400

    Add files via upload

---
+++ stanog.py
@@ -0,0 +1,14 @@
+import os 
+print("""
+______________________________
+      CODER => H4WK OFCX 
+      YOUTUBE => HAWK DEFACER 
+      İNSTAGRAM => hawkofcx
+      GİTHUB : HAWK-UNİTY
+_____________________________
+Resim içine yazı gizlemek için mi geldin :D
+
+""")  
+veri = input("Gizlenecek yazıyı gir => ")
+veri2 = input("resim dosyasının adı (örnek.jpg) => ")
+os.system("echo -n "+ veri + " >> "+veri2) 
